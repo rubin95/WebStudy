405888b241ce99fab442987c5f030ab991ab3229
Yoobin JO <dbqlsdl1234@naver.com>
2020-01-03 02:18:02 +0900

    js, Promise에 대한 마지막 공부

--- JavaScript/promiseTest.js
+++ JavaScript/promiseTest.js
@@ -45,12 +45,65 @@ function p() {
     });
 }
 
-console.log('nothing doing');
-p()
-    .then((message) => {
-        console.log('이행중인 상태', message);
-    })
-    .catch((error) => {
-        console.log('rejected', error);
-    });
+// console.log('nothing doing');
+// p()
+//     .then((message) => {
+//         console.log('이행중인 상태', message);
+//     })
+//     .catch((error) => {
+//         console.log('rejected', error);
+//     })
+//     .finally(() => {
+//         console.log("end");
+//     });
 
+
+function c(callback) {
+    setTimeout(() => {
+        callback();
+    }, 1000);
+}
+
+// c(() => {
+//     console.log("1000ms 후 콜백");
+// });
+// c(() => {
+//     c(() => {
+//         console.log("1000ms 후 콜백");
+//     });
+// });
+// c(() => {
+//     console.log("1000ms 후 콜백");
+// });
+
+function pro() {
+    return new Promise((resolve, reject) => {
+       setTimeout(() => {
+           console.log(1);
+           resolve();
+       }, 1000);
+    });
+}
+// pro().then(() => {
+//     return pro();
+// })
+//     .then(() => pro())
+//     .then(pro());
+
+// Promise.resolve(new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('foo', 'data');
+//     }, 1000);
+// })).then((data1, data2) => {
+//     console.log(data1, data2);
+// })
+//
+// Promise.resolve('bar').then(data => {
+//     console.log(data);
+// });
+
+Promise.reject(new Error('reason')).then(error => {
+
+}).catch(error => {
+    console.log(error);
+});
