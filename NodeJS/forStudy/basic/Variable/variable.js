3a1e98729356e77a235be6dee40a0f23a1aaf33e
Yoobin JO <dbqlsdl1234@naver.com>
2020-01-25 20:11:09 +0900

    변수 생성과 객체에 대해 공부하고 객체에 값을 할당하는 방법에 대해 공부

---
+++ NodeJS/forStudy/basic/Variable/variable.js
@@ -0,0 +1,10 @@
+var Person = {};
+
+Person['age'] = 26;
+Person['name'] = 'Rubin';
+Person.mobile = '010-1234-5678';
+
+console.log(Person.name);
+console.log(Person.age);
+console.log(Person['mobile']);
+console.log(Person.mobile);
