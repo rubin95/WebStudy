d88caab8f84366d1e85a2b4bec2f7d962188bfbd
Yoobin JO <dbqlsdl1234@naver.com>
2020-01-25 17:08:38 +0900

    Do it! Node.js 기본 공부 중

---
+++ NodeJS/forStudy/basic/first.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// var result = 0;
+//
+// console.time('durationSum');
+//
+// for(var i = 1; i <= 1000000000; i++) {
+//     result += i;
+// }
+//
+// console.timeEnd('durationSum');
+// console.log(result)
+
+var res = 0;
+for(var i = 0; i < 101; i++) {
+    res += i;
+}
+
+console.log(res);
+
+console.log(101 * 100 / 2);
+
