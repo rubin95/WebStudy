b73bd99a705b9ab6f8d7278ddfa40072c122406e
Yoobin JO <dbqlsdl1234@naver.com>
2020-01-29 22:13:31 +0900

    간이 웹서버를 만들어서 instream -> res pipe 테스트

---
+++ NodeJS/forStudy/2. NodeDefaultFunction/File/httpStream.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    var instream = fs.createReadStream('./output.txt', 'utf8');
+    console.log(instream);
+    instream.pipe(res);
+});
+
+server.listen(7001, '127.0.0.1', function () {
+    console.log('now running');
+});
