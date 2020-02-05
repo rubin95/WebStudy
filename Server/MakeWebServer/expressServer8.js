1e0cf27224859784b9d755d78253419696fcb67d
Rubin <dbqlsdl1234@naver.com>
2020-02-05 22:00:05 +0900

    라우팅을 하는 기초적인 방법에 대하여 공부

---
+++ Server/MakeWebServer/expressServer8.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+var bodyParser = require('body-parser'),
+    staticM = require('serve-static');
+
+var app = express();
+var router = express.Router();
+
+// 기본 속성 설정
+app.set('port', process.env.PORT || 3000);
+
+// body-parser를 사용해 application/x-www-form-urlencode 파싱
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// body-parser를 이용해 application/json 파싱
+app.use(bodyParser.json());
+
+app.use('', staticM(path.join(__dirname, 'public')));
+
+router.route('/process/login').post(function (req, res) {
+    console.log('/process/login 처리함');
+
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+
+    res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+    res.write('<h1>Express 서버에서 응답한 결과입니다. </h1>');
+    res.write('<div><p>Param id: ' + paramId + '</p></div>');
+    res.write('<div><p>Param password: ' + paramPassword + '</p></div>');
+    res.write('<br><br><a href="/login2.html">로그인 페이지로 돌아가기</a>');
+    res.end();
+});
+
+app.use('/', router);
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('server start');
+});
