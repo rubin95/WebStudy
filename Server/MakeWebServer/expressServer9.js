e11b0b3873b010213f3733223a27c8164ae358db
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-09 19:20:48 +0900

    에러메시지를 표시하는 방법에 대하여 공부

---
+++ Server/MakeWebServer/expressServer9.js
@@ -0,0 +1,55 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    expressErrorHandler = require('express-error-handler');
+
+var bodyParser = require('body-parser'),
+    staticM = require('serve-static');
+
+var app = express();
+var router = express.Router();
+
+var errorHandler = expressErrorHandler({
+    static: {
+        '404': './public/404.html'
+    }
+});
+
+// 기본 속성 설정
+app.set('port', process.env.PORT || 3000);
+
+// body-parser를 사용해 application/x-www-form-urlencode 파싱
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// body-parser를 이용해 application/json 파싱
+app.use(bodyParser.json());
+
+app.use('', staticM(path.join(__dirname, 'public')));
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+router.route('/process/login/:name').post(function (req, res) {
+    console.log('/process/login/:name 처리함');
+
+    var paramName = req.params.name;
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+
+    res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+    res.write('<h1>Express 서버에서 응답한 결과입니다. </h1>');
+    res.write('<div><p>Param name: ' + paramName + '</p></div>');
+    res.write('<div><p>Param id: ' + paramId + '</p></div>');
+    res.write('<div><p>Param password: ' + paramPassword + '</p></div>');
+    res.write('<br><br><a href="/login2.html">로그인 페이지로 돌아가기</a>');
+    res.end();
+});
+
+app.use('/', router);
+
+// app.all('*', function (req, res) {
+//     res.status(404).send('<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>');
+// });
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('server start');
+});
