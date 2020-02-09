b5dc6ea0ec0b52c2f0d7ee722f32bbfc8a0cf215
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-09 11:59:47 +0900

    커밋

--- Server/MakeWebServer/expressServer8.js
+++ Server/MakeWebServer/expressServer8.js
@@ -19,14 +19,16 @@ app.use(bodyParser.json());
 
 app.use('', staticM(path.join(__dirname, 'public')));
 
-router.route('/process/login').post(function (req, res) {
-    console.log('/process/login 처리함');
+router.route('/process/login/:name').post(function (req, res) {
+    console.log('/process/login/:name 처리함');
 
+    var paramName = req.params.name;
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
 
     res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
     res.write('<h1>Express 서버에서 응답한 결과입니다. </h1>');
+    res.write('<div><p>Param name: ' + paramName + '</p></div>');
     res.write('<div><p>Param id: ' + paramId + '</p></div>');
     res.write('<div><p>Param password: ' + paramPassword + '</p></div>');
     res.write('<br><br><a href="/login2.html">로그인 페이지로 돌아가기</a>');
@@ -34,6 +36,10 @@ router.route('/process/login').post(function (req, res) {
 });
 
 app.use('/', router);
+
+app.all('*', function (req, res) {
+    res.status(404).send('<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>');
+});
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('server start');
