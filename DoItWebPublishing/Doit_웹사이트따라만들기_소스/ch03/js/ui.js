6fd4c916ed12ac880f9559e9748be1dcc11608d6
Rubin.Jo <dbqlsdl1234@naver.com>
2020-12-30 15:55:05 +0900

    JQuery를 통해 화면 전환 적용

--- DoItWebPublishing/Doit_웹사이트따라만들기_소스/ch03/js/ui.js
+++ DoItWebPublishing/Doit_웹사이트따라만들기_소스/ch03/js/ui.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
-	
+    $("#container").addClass("start");
+	$("nav li").click(function () {
+	    $("#container").css("max-width", "100%");
+        var id = $(this).attr("data-rol");
+	    $("nav li").removeClass("on");
+	    $(this).addClass("on");
+        $(".content").removeClass("prev this next");
+        $("#" + id).addClass("this").prevAll().addClass("prev");
+        $("#" + id).nextAll().addClass("next");
+    });
+
+	$(".logo_box").click(function () {
+        $("nav li").removeClass("on");
+	    $(".content").removeClass("prev this next");
+	    $("#container").css("max-width", "1200px");
+    });
 });
 
